89572925b1b29aad746de3cebe7e4d97ba1c318b
Rodrick <jesprikovic@gmail.com>
2022-12-14 18:50:26 -0500

    Gameboard factory function blueprint

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,8 +1,9 @@
-import createShip from "../app/modules/ship";
+import createShip from "../app/modules/ship.js";
+import createGameboard from "../app/modules/gameboard.js";
 
 describe("Gameboard Testing", () => {
     let ship = createShip(4);
-    let gameboard = null;
+    let gameboard = createGameboard();
 
     test("Gameboard can place ship at specific location", () => {
         gameboard.placeShip(ship, [0, 4], [4, 4]);
