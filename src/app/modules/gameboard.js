4b814c356434a17530bf3a49f05c83db2fe892a0
Rodrick <jesprikovic@gmail.com>
2022-12-15 12:33:40 -0500

    Add function to create 10x10 2D Array

--- src/app/modules/gameboard.js
+++ src/app/modules/gameboard.js
@@ -1,7 +1,20 @@
 export default function createGameboard() {
     return {
+        spaces: create10x10Grid(),
         shipAt() {},
         placeShip() {},
         receiveAttack() {},
     };
+}
+
+function create10x10Grid() {
+    let spaces = [];
+    for (let i = 0; i < 10; i++) {
+        let space = [];
+        for (let j = 0; j < 10; j++) {
+            space.push("");
+        }
+        spaces.push(space);
+    }
+    return spaces;
 }
